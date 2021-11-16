d12dfb0760ec371acac9c7597b7dc97cca307ed8
Kreighton <91473736+Kreighton@users.noreply.github.com>
2021-11-16 16:17:13 +0200

    Update 1.py

--- HT_1/1.py
+++ HT_1/1.py
@@ -1,1 +1,9 @@
+#Task 1
+print("Enter the numbers (separate with comma):")
+MainInput = input()
 
+mList = list(MainInput.split(","))
+mTulpe = tuple(MainInput.split(","))
+
+print(f"List: {mList}")
+print(f"Tuple: {mTulpe}")
