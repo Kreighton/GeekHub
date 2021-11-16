9fa9c2259e60ceec5ba14192e8af46ce33ddffb0
Kreighton <91473736+Kreighton@users.noreply.github.com>
2021-11-16 16:56:30 +0200

    Create 3.py

---
+++ HT_1/3.py
@@ -0,0 +1,9 @@
+#3. Write a script to sum of the first n positive integers.
+
+InputList = input("Enter the integers for summing (split with space): ")
+InputList.split(" ").sort()
+SumList = []
+for i in range(len(InputList)):
+    if InputList[i].isdigit():
+       SumList.append(InputList[i])
+print(sum(map(int, SumList)))
