55c541962ae31a62470401f056163d457df23474
Kreighton <91473736+Kreighton@users.noreply.github.com>
2021-11-16 18:16:40 +0200

    Update 2.py

--- HT_1/2.py
+++ HT_1/2.py
@@ -6,8 +6,8 @@
 #        {'Black', 'White'}
 
 
-color_list_1 = set(["White", "Black", "Red"])
-color_list_2 = set(["Red", "Green"])
+color_list_1 = set(map(str, input("Enter first color list: ").split(", ")))
+color_list_2 = set(map(str, input("Enter second color list: ").split(", ")))
 print(f"list 1: {color_list_1}")
 print(f"list 2: {color_list_2}")
 print(f"List difference: {color_list_1.difference(color_list_2)}")
