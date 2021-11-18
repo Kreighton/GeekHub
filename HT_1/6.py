4b7ab7dcb2784fb913dfbbe855805afa6783dbc4
Kreighton <91473736+Kreighton@users.noreply.github.com>
2021-11-18 15:58:48 +0200

    Update 6.py

--- HT_1/6.py
+++ HT_1/6.py
@@ -3,9 +3,23 @@
 #        3 -> [1, 5, 8, 3] : True
 #        -1 -> (1, 5, 8, 3) : False
 
+TypeOfInput = ""
 
-# container = tuple(input("Container values: ").split(" "))
-container = list(input("Container values: ").split(" "))
+while TypeOfInput != "1" or TypeOfInput != "2":
+    TypeOfInput = input("List or Tuple? (1 - List, 2 - Tuple): ")
+    if TypeOfInput.isdigit():
+        if int(TypeOfInput) <= 0 or int(TypeOfInput) > 2:
+            print("Enter correct value!")
+        else: 
+            break
+    else: 
+        print("Enter correct value!")
+
+if TypeOfInput == "1":
+    container = list(input("You choose 1. Enter list values: ").split(" "))
+elif TypeOfInput == "2":
+    container = tuple(input("You choose 2. Enter tuple values: ").split(" "))
+
 SearchedValue = input("Enter searched value: ")
 
 print(f"{SearchedValue} -> {container}: {SearchedValue in container}")
