454d7284ef4d4ce8cf97128891f4fa2f1e737cc8
Kreighton <91473736+Kreighton@users.noreply.github.com>
2021-11-18 16:02:41 +0200

    Update 6.py

--- HT_1/6.py
+++ HT_1/6.py
@@ -17,9 +17,9 @@ while TypeOfInput != "1" or TypeOfInput != "2":
 
 
 if TypeOfInput == "1":
-    container = list(input("You choose 1. Enter list values: ").split(" "))
+    container = list(input("You choose 1. Enter list values, separated with spaces: ").split(" "))
 elif TypeOfInput == "2":
-    container = tuple(input("You choose 2. Enter tuple values: ").split(" "))
+    container = tuple(input("You choose 2. Enter tuple values, separated with spaces: ").split(" "))
 
 SearchedValue = input("Enter searched value: ")
 
