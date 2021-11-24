8a25accca81bbc063332f402b5d96c4bc8c591ea
Kreighton <91473736+Kreighton@users.noreply.github.com>
2021-11-24 12:29:21 +0200

    Update 7.py

--- HT_3/7.py
+++ HT_3/7.py
@@ -2,20 +2,25 @@
 
 def calculator_custom(op1, op2, sign_mark):
     try:
+        err_counter = 0
         op1_dig = int(op1)
         op2_dig = int(op2)
         calc_lib = {"+": op1_dig + op2_dig, "-": op1_dig - op2_dig, "*": op1_dig * op2_dig, "/": op1_dig / op2_dig}
-        return f"{op1} {sign_mark} {op2} = {calc_lib[sign_mark]}"
-    except:
-        return "Enter correct values!"
+        return f"{op1} {sign_mark} {op2} = {calc_lib[sign_mark]}", err_counter
+    except ValueError:
+        err_counter = 1
+        return "Enter correct values!", err_counter
+    except ZeroDivisionError as zero_div_err:
+        err_counter = 1
+        return f"Error! {zero_div_err}", err_counter
 
 
 while True:
     first_num = input("Enter first number: ")
     sec_num = input("Enter second number: ")
     sign_mark = input("Enter operation (+, -, *, /): ")
-    result = calculator_custom(first_num, sec_num, sign_mark)
-    if result != "Enter correct values!":
+    result, error_value = calculator_custom(first_num, sec_num, sign_mark)
+    if error_value == 0:
         print(result)
         break
     else:
