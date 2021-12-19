ca0e68368ed1deb25c520e86a6fd91f0ea05d3a1
Kreighton <91473736+Kreighton@users.noreply.github.com>
2021-12-19 15:59:23 +0200

    Update 1.py

--- HT_10/1.py
+++ HT_10/1.py
@@ -1,17 +1,44 @@
-"""Перепишіть програму-банкомат на використання бази даних для збереження всих даних.
-Використовувати БД sqlite3 та натівний Python.
-Дока з прикладами: https://docs.python.org/3/library/sqlite3.html
-Туторіал (один із): https://www.sqlitetutorial.net/sqlite-python/
-Для уніфікації перевірки, в базі повинні бути 3 користувача:
-  ім'я: user1, пароль: user1
-  ім'я: user2, пароль: user2
-  ім'я: admin, пароль: admin (у цього коритувача - права інкасатора)
+"""1. Доповніть програму-банкомат наступним функціоналом:
+   - новий пункт меню, який буде виводити поточний курс валют (API Приватбанк)
+2. Написати скрипт, який буде приймати від користувача назву валюти і початкову дату.
+   - Перелік валют краще принтануть.
+   - Також не забудьте указати, в якому форматі коритувач повинен ввести дату.
+   - Додайте перевірку, чи введена дата не знаходиться у майбутньому ;)
+   - Також перевірте, чи введена правильна валюта.
+   Виконуючи запроси до API архіву курсу валют Приватбанку, вивести інформацію про зміну
+   курсу обраної валюти (Нацбанк) від введеної дати до поточної. Приблизний вивід наступний:
+   Currency: USD
+   Date: 12.12.2021
+   NBU:  27.1013   -------
+   Date: 13.12.2021
+   NBU:  27.0241   -0,0772
+   Date: 14.12.2021
+   NBU:  26.8846   -0,1395
+3. Конвертер валют. Прийматиме від користувача назву двох валют і суму (для першої).
+   Робить запрос до API архіву курсу валют Приватбанку (на поточну дату) і виконує
+   конвертацію введеної суми з однієї валюти в іншу.
+P.S. Не забувайте про файл requirements.txt
+P.P.S. Не треба сходу ДДОСить Приватбанк - додайте хоча б по 0.5 секунди між запросами.
+       Хоч у них і не написано за троттлінг, але будьмо чемними ;)
+Інформація для виконання:
+- документація API Приватбанка:
+  - архів курсів: https://api.privatbank.ua/#p24/exchangeArchive
+  - поточний курс: https://api.privatbank.ua/#p24/exchange
+- інформація про використання форматування дати в Python: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+- модуль requests: https://docs.python-requests.org/en/latest/
 
    """
 
 import json
 import csv
 import sqlite3
+import requests
+from datetime import date, timedelta, datetime
+import time
+class WrongDateInput(Exception):
+    pass
+class CurrencyNotExists(Exception):
+        pass
 
 class InsufficientBanknotes(Exception):
     pass
@@ -209,16 +236,109 @@ def add_bankomat_funds():
         return 'Такой банкноты не существует! Возврат в главное меню'
 
 
+def daterange(custom_date):
+    for i in range(int((date.today() - custom_date).days) + 1):
+        yield custom_date + timedelta(i)
+
+
+def getcurrency_history(custom_date, currency):
+    currencies = ['USD', 'EUR', 'RUR', 'CHF', 'GBP', 'PLZ', 'SEK', 'XAU', 'CAD']
+    try:
+        if currency not in currencies:
+            raise CurrencyNotExists()
+        dd = int(custom_date[0:2])
+        mm = int(custom_date[3:5])
+        yyyy = int(custom_date[6:])
+        if date(yyyy, mm, dd) > date.today():
+            raise WrongDateInput()
+        print(f'Currency: {currency}')
+        for i in daterange(date(yyyy, mm, dd)):
+            time.sleep(1)
+            url = f'https://api.privatbank.ua/p24api/exchange_rates?json&date={i.strftime("%d.%m.%Y")}'
+            currency_hist_data = requests.get(url=url).json()['exchangeRate']
+            print(f'Date: {i.strftime("%d.%m.%Y")}')
+            if i == date.today():
+                print(''.join([f'NBU:    {currency_hist_data[cur]["saleRateNB"]}    -------\n'
+                           for cur in range(1, len(currency_hist_data)) if currency_hist_data[cur]["currency"] == currency]))
+
+            else:
+                print(''.join([f'NBU:    {currency_hist_data[cur]["saleRateNB"]}    {float(currency_hist_data[cur]["purchaseRateNB"] - currency_hist_data[cur]["saleRateNB"])}\n'
+                           for cur in range(1, len(currency_hist_data)) if currency_hist_data[cur]["currency"] == currency]))
+        return 'Done!'
+    except CurrencyNotExists:
+        return 'Такой валюты не существует! Возврат в главное меню.'
+    except WrongDateInput:
+        return 'Неверно введенная дата! Возврат в главное меню'
+    except ValueError:
+        return 'Неверно введенная дата! Возврат в главное меню'
+
+
+def curr_converter(curr_from, curr_to, curr_value):
+    try:
+        converted_value = 0
+        curr_value = float(curr_value)
+        result = 0
+        func_currency = ''
+        move_hist = ''
+        move = ''
+        curs_for_sale = ['USD', 'EUR', 'RUR']
+        if curr_from == curr_to:
+            result = curr_value
+        elif curr_from in curs_for_sale and curr_to == 'UAH':
+            move = 'sale'
+            move_hist = 'saleRateNB'
+            func_currency = curr_from
+        elif curr_from == 'UAH' and curr_to in curs_for_sale:
+            move = 'buy'
+            move_hist = 'purchaseRateNB'
+            func_currency = curr_to
+        else:
+            raise CurrencyNotExists()
+        if move:
+            url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+            temp = requests.get(url=url).json()
+            converted_value = float([temp[i][move] for i in range(len(temp)) if temp[i]['ccy'] == func_currency][0])
+            if converted_value == 0:
+                yesterdays_date = (date.today() - timedelta(days=1)).strftime("%d.%m.%Y")
+                url = f'https://api.privatbank.ua/p24api/exchange_rates?json&date={yesterdays_date}'
+                temp = requests.get(url=url).json()['exchangeRate']
+                converted_value = float([temp[i][move_hist] for i in range(1, len(temp)) if temp[i]['currency'] == func_currency][0])
+            if move == 'buy':
+                result = curr_value / converted_value
+            else:
+                result = curr_value * converted_value
+        return f"{curr_value} {curr_from} в {curr_to} = {result} {curr_to}"
+    except CurrencyNotExists:
+        return 'Вы ввели неверную валюту! Возврат в главное меню.'
+    except ValueError:
+        return 'Введеная неверная валюта или сумма для обмена! Возврат в главное меню.'
+
+
+def getcurrency_current():
+    url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+    a = requests.get(url=url).json()
+    for i in range(len(a)):
+        a[i]["buy"] = "%.2f" % float(a[i]["buy"])
+        a[i]["sale"] = "%.2f" % float(a[i]["sale"])
+    result = ''.join([f'{a[i]["ccy"]} к {a[i]["base_ccy"]}:\nПродажа: {a[i]["buy"]}\nПокупка: '
+                      f'{a[i]["sale"]}\n{"-" * 40}\n' for i in range(len(a))])
+    return result
+
+
 def start(login, incasator=False):
     try:
+        print("-" * 30)
         while True:
             if not incasator:
-                user_operation = int(input(f'{"-" * 30}\nДобро пожаловать, {login}! Выберите операцию:'
+                user_operation = int(input(f'Добро пожаловать, {login}! Выберите операцию:'
                                            '\n1. Просмотреть баланс'
                                            '\n2. Пополнение баланса'
                                            '\n3. Снятие баланса'
-                                           '\n4. Выход\n'))
-                if user_operation in range(1, 5):
+                                           '\n4. Курс валют'
+                                           '\n5. Проверить историю курса валют'
+                                           '\n6. Конвертер валют'
+                                           '\n7. Выход\n'))
+                if user_operation in range(1, 8):
                     if user_operation == 1:
                         print(f'{"-" * 30}\n{check_balance(login)}')
                     elif user_operation == 2:
@@ -226,7 +346,20 @@ def start(login, incasator=False):
                     elif user_operation == 3:
                         print(f'{"-" * 30}\n{drop_balance(login)}')
                     elif user_operation == 4:
-                        print('4 - Выход')
+                        print(f'{"-" * 30}\n{getcurrency_current()}')
+                    elif user_operation == 5:
+                        print('5 - История курса валют.')
+                        currency = input('Введите дату (формат дд.мм.гггг): ')
+                        custom_date = input('Введите валюту для просмотра (USD, EUR, RUR, CHF, GBP, PLZ, SEK, XAU, CAD): ')
+                        print(f'{"-" * 30}\n{getcurrency_history(currency, custom_date)}')
+                    elif user_operation == 6:
+                        print('6 - Конвертер валют. Конверсия может быть проведена только между UAH и другими валютами.')
+                        curr_from = input('Введите стартовую валюту (Доступные валюты: UAH, USD, RUR, USD): ')
+                        curr_to = input('Введите валюту для конвертирования (Доступные валюты: UAH, USD, RUR, USD): ')
+                        curr_value = input('Введите сумму для конвертирования: ')
+                        print(f'{"-" * 30}\n{curr_converter(curr_from, curr_to, curr_value)}')
+                    elif user_operation == 7:
+                        print('7 - Выход')
                         return
                 else:
                     raise WrongOperationError()
@@ -270,5 +403,3 @@ while True:
     else:
         try_counter -= 1
         print(f'{"-" * 30}\nОшибка ввода логина/пароля. Осталось {try_counter} попыток')
-
-
