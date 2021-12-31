6755efb8e4aa888fb41a0fd199ce5020e606b879
Kreighton <91473736+Kreighton@users.noreply.github.com>
2021-12-31 13:41:38 +0200

    Create 3.py

---
+++ HT_13/3.py
@@ -0,0 +1,22 @@
+'''
+3. Напишіть програму, де клас «геометричні фігури» (figure) містить властивість color з початковим значенням white і метод для зміни кольору фігури, 
+а його підкласи «овал» (oval) і «квадрат» (square) містять методи __init__ для завдання початкових розмірів об'єктів при їх створенні.'''
+
+class Figure(object):
+    color = 'white'
+    
+class Oval(Figure):
+        
+    def __init__(self, far_distance, close_distance):
+        self.far_distance = far_distance
+        self.close_distance = close_distance
+            
+            
+class Square(Figure):
+        
+    def __init__(self, side):
+        self.side = side
+            
+sq_1 = Square(4)
+
+print(sq_1.color, sq_1.side)
