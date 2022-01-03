cf90b9042b6e65d6c99ec7536d39889987b862d4
Kreighton <91473736+Kreighton@users.noreply.github.com>
2022-01-03 13:03:49 +0200

    Update 5.py

--- HT_13/5.py
+++ HT_13/5.py
@@ -10,7 +10,7 @@
 create table books(id integer primary key autoincrement, book_name text, author text, pages integer, janra text, edition text
 '''
 import sqlite3
-
+import os
 
 class Book(object):
 
@@ -21,21 +21,72 @@ class Book(object):
         self.janra = args[3]
         self.edition = args[4]
         
+    def __str__(self):
+        return f'{[self.book_name, self.author, self.pages, self.janra, self.edition]}'
 
-    def show_book(self):
-        print(self.book_name, self.author, self.pages, self.janra, self.edition)
-        
-class Dictionary(Book):
+class Library(object):
+    if os.path.isfile('main.db'):
+        os.remove('main.db')
+    con = sqlite3.connect('main.db')
+    cur = con.cursor()
+    cur.execute('CREATE TABLE books(id integer primary key autoincrement, book_name text, author text, pages integer, janra text, edition text, copies integer)')
+    con.commit()
+    con.close()
 
-    def __init__(self, lang_to, lang_in, *args):
-        self.lang_to = lang_to
-        self.lang_in = lang_in
-        super(Dictionary, self).__init__(*args)
+    def copies_detect(self, name):
+        con = sqlite3.connect('main.db')
+        cur = con.cursor()
+        copy = cur.execute('select copies FROM books WHERE book_name=?', name)
+        con.close()
+        return copy
+    #тут ошибка, не находит copies_detect
+    def add_book(self, book):
+        app_items = (book.book_name, book.author, book.pages, book.janra, book.edition)
+        con = sqlite3.connect('main.db')
+        cur = con.cursor()
+        book_copy = copies_detect(book.book_name)
+        cur.execute('INSERT INTO books(book_name, author, pages, janra, edition) VALUES(?,?,?,?,?)', app_items)
+        con.commit()
+        con.close()
+         
+    
 
-class 
+    def pop_book(self, name):
+        try:
+            con = sqlite3.connect('main.db')
+            cur = con.cursor()
+            find_book = cur.execute('SELECT book_name FROM books WHERE book_name=?', name)
+            if not find_book:
+                raise ValueError()
+            cur.execute('DELETE FROM books WHERE book_name=?', name)
+            con.commit()
+            con.close()
+        except ValueError:
+            print('Wrong book name!')
+            return
+
+
+    def find_book(self, name='', author=''):
+        try:
+            con = sqlite3.connect('main.db')
+            cur = con.cursor()
+            if not name and not author:
+                raise ValueError()
+            elif not name:
+                result = cur.execute('SELECT book_name, author, pages, janra, edition FROM books WHERE author=?', (author,)).fetchone()
+            else:
+                result = cur.execute('SELECT book_name, author, pages, janra, edition FROM books WHERE book_name=?', (name,)).fetchone()
+            return result
+        except ValueError:
+            return ('Please enter book name or book author!')
+            
+
+
 book_1 = Book('Eye of ctulhu', 'Hovard Lovecraft', 314, 'Horror', 'asdasd')
+print(book_1)
 
-book_1.show_book()
+lib = Library()
 
-book_2 = Dictionary('English', 'German', 'English-German general dictionary', 'Someone', 890, 'Dictionary', 'asdasd')
-book_2.show_book()
+lib.add_book(book_1)
+lib.add_book(book_1)
+print(lib.find_book(name='Eye of ctulhu'))
