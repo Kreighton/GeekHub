868b0047f2d885c6eaa2cde8b667717ba1636952
Kreighton <91473736+Kreighton@users.noreply.github.com>
2022-01-06 12:33:49 +0200

    Update 4.py

--- HT_13/4.py
+++ HT_13/4.py
@@ -1,27 +1,34 @@
 '''
-4. Видозмініть програму так, щоб метод __init__ мався в класі «геометричні фігури» та приймав кольор фігури при створенні екземпляру, а методи __init__ підкласів доповнювали його та додавали початкові розміри'''
+4. Видозмініть програму так, щоб метод __init__ мався в класі «геометричні фігури»
+та приймав кольор фігури при створенні екземпляру,
+а методи __init__ підкласів доповнювали його та додавали початкові розміри'''
 
 class Figure(object):
-    
+
     def __init__(self, color):
         self.color = color
-    
+
+    def change_color(self, color):
+        self.color = color
+
 class Oval(Figure):
-        
+
     def __init__(self, far_distance, close_distance, color='white'):
         self.far_distance = far_distance
         self.close_distance = close_distance
         super(Oval, self).__init__(color=color)
-            
+
 class Square(Figure):
-        
+
     def __init__(self, side, color='white'):
         self.side = side
         super(Square, self).__init__(color=color)
-            
+
+
 sq_1 = Square(4)
-print(sq_1.color, sq_1.side)
+print(sq_1.side, sq_1.color)
 
 ov_1 = Oval(5, 2, 'black')
-
+print(ov_1.far_distance, ov_1.close_distance, ov_1.color)
+ov_1.change_color('yellow')
 print(ov_1.far_distance, ov_1.close_distance, ov_1.color)
