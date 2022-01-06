452bb757144185c52fad54e7a59e242597ee50e7
Kreighton <91473736+Kreighton@users.noreply.github.com>
2022-01-06 12:31:13 +0200

    Update 3.py

--- HT_13/3.py
+++ HT_13/3.py
@@ -1,22 +1,29 @@
 '''
-3. Напишіть програму, де клас «геометричні фігури» (figure) містить властивість color з початковим значенням white і метод для зміни кольору фігури, 
+3. Напишіть програму, де клас «геометричні фігури» (figure) містить властивість color з початковим значенням white і метод для зміни кольору фігури,
 а його підкласи «овал» (oval) і «квадрат» (square) містять методи __init__ для завдання початкових розмірів об'єктів при їх створенні.'''
+
 
 class Figure(object):
     color = 'white'
-    
+
+    def change_color(self, color):
+        self.color = color
+
+
 class Oval(Figure):
-        
+
     def __init__(self, far_distance, close_distance):
         self.far_distance = far_distance
         self.close_distance = close_distance
-            
-            
+
+
 class Square(Figure):
-        
+
     def __init__(self, side):
         self.side = side
-            
-sq_1 = Square(4)
 
+
+sq_1 = Square(4)
+print(sq_1.color, sq_1.side)
+sq_1.change_color('black')
 print(sq_1.color, sq_1.side)
