636b25f224abef36ab9f3a4687ff57126cefdf36
Kreighton <91473736+Kreighton@users.noreply.github.com>
2022-02-09 21:50:57 +0200

    update success cart message

--- HT_20/products/urls.py
+++ HT_20/products/urls.py
@@ -4,8 +4,11 @@ from django.conf.urls.static import static
 
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('<slug:slug>', views.homepage, name='homepage_category'),
     path('product/<slug:slug>', views.product_details, name='product_details'),
+    path('<str:slug>/add', views.add_to_cart, name='add_to_cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- HT_20/products/views.py
+++ HT_20/products/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Product, ProductCategory
 
 
 def homepage(request, slug=None):
-
     data = Product.objects.filter(status=True)
+
     if slug:
         category = get_object_or_404(ProductCategory, slug=slug)
         data = data.filter(product_category=category)
@@ -15,10 +18,16 @@ def homepage(request, slug=None):
     }
     return render(request, 'products/homepage.html', context)
 
+
 def product_details(request, slug):
     data = Product.objects.get(slug=slug)
     context = {
         'data': data,
     }
-
     return render(request, 'products/product_details.html', context)
+
+
+@login_required
+def add_to_cart(request, slug):
+    messages.success(request, 'Product was added!')
+    return redirect('/')
